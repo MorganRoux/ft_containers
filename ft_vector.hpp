12705c70f09ab0dcd33dac442946902450062105
Morgan Roux <roux.morgan@gmail.com>
2021-07-10 21:57:27 +0200

    vector wip

--- ft_vector.hpp
+++ ft_vector.hpp
@@ -6,13 +6,14 @@
 /*   By: mroux <mroux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/10 21:07:55 by mroux             #+#    #+#             */
-/*   Updated: 2021/07/10 21:10:58 by mroux            ###   ########.fr       */
+/*   Updated: 2021/07/10 21:57:15 by mroux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FTVECTOR_HPP
 #define FTVECTOR_HPP
 #include <iostream>
+#include <stdexcept>
 #include "ft_iterator.hpp"
 namespace ft
 {
@@ -31,16 +32,53 @@ namespace ft
 		typedef Iterator<std::random_access_iterator_tag, const T>	const_iterator; 		// a random access iterator to const value_type
 		typedef ReverseIterator<iterator>							reverse_iterator;
 		typedef ReverseIterator<const_iterator>						const_reverse_iterator;
-		typedef ptrdiff_t											difference_type; 		//a signed integral type, identical to: iterator_traits<iterator>::difference_type
+		typedef ptrdiff_t											difference_type;		//a signed integral type, identical to: iterator_traits<iterator>::difference_type
 		typedef size_t												size_type;
 
-		explicit vector(const allocator_type &alloc = allocator_type());
-		explicit vector(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type());
+		// Constructor - Destructor
+		explicit vector(const allocator_type &alloc = allocator_type()):
+			_alloc(alloc), _size(0)
+		{
+			_v = _alloc.allocate(_size, 0);
+		}
+
+		explicit vector(size_type n, const value_type &val = value_type(), const allocator_type &alloc = allocator_type()):
+			_alloc(alloc), _size(n)
+		{
+			_v = _alloc.allocate(_size, 0);
+			for (size_type i = 0; i < _size; i++)
+				_alloc.construct(&_v[i], val);
+		}
+
 		template <class InputIterator>
 		vector(InputIterator first, InputIterator last, const allocator_type &alloc = allocator_type());
-		vector(const vector &x);
-		~vector();
-		vector &operator=(const vector &x);
+
+		vector(const vector &x):
+			_alloc(x._alloc), _size(x._size)
+		{
+			_v = _alloc.allocate(_size, 0);
+			for (size_type i = 0; i < _size; i++)
+				_alloc.construct(&_v[i], x[i]);
+		}
+
+		~vector()
+		{
+			for (size_type i = 0; i < _size; i++)
+				_alloc.destroy(&_v[i]);
+			_alloc.deallocate(_v, _size);
+		}
+
+		vector &operator=(const vector &x)
+		{
+			for (size_type i = 0; i < _size; i++)
+				_alloc.destroy(&_v[i]);
+			_alloc.deallocate(_v, _size);
+			_alloc = x._alloc;
+			_size = x._size;
+			_v = _alloc.allocate(_size, 0);
+			for (size_type i = 0; i < _size; i++)
+				_alloc.construct(&_v[i], x[i]);
+		}
 
 		//Iterators
 		iterator begin();
@@ -53,18 +91,40 @@ namespace ft
 		const_reverse_iterator rend() const;
 
 		// Capacity
-		size_type size() const;
-		size_type max_size() const;
+		size_type size() const{ return _size; }
+		size_type max_size() const
+		{
+			size_type max_size = 0;
+			try {
+				max_size = _alloc.max_size();
+			} catch (std::exception& e){
+				std::cout << "Error in vector.max_size() :" << e.what() << std::endl;
+			}
+			return (max_size);
+		}
+
 		void resize(size_type n, value_type val = value_type());
 		size_type capacity() const;
-		bool empty() const;
+		bool empty() const { return (_size == 0); }
 		void reserve(size_type n);
 
 		// Element access
-		reference operator[](size_type n);
-		const_reference operator[](size_type n) const;
-		reference at(size_type n);
-		const_reference at(size_type n) const;
+		reference operator[](size_type n) { return *(_v + n); }
+
+		const_reference operator[](size_type n) const { return *(_v + n); }
+
+		reference at(size_type n) throw(std::out_of_range)
+		{
+			if (n >= _size)
+				throw std::out_of_range("Out of range error in vector.at()");
+			return *(_v + n);
+		}
+		const_reference at(size_type n) const throw(std::out_of_range)
+		{
+			if (n >= _size)
+				throw std::out_of_range("Out of range error in vector.at() const");
+			return *(_v + n);
+		}
 		reference front();
 		const_reference front() const;
 		reference back();
@@ -88,7 +148,9 @@ namespace ft
 		allocator_type get_allocator() const;
 
 	private:
-		T*	_v;
+		T*			_v;
+		Alloc		_alloc;
+		size_type	_size;
 	};
 
 	// relational operators
--- main.cpp
+++ main.cpp
@@ -2,5 +2,26 @@
 
 int main(void)
 {
+	ft::vector<int> v1;
+	ft::vector<int> v2(6);
+	v2[2] = 7;
+	const ft::vector<int>v3(v2);
+	std::cout << v2[3] << " - " << v2.at(3) << std::endl;
+	std::cout << v2[2] << std::endl;
+	std::cout << v3[2] << std::endl;
+	try {
+		v2.at(10);
+	}
+	catch(std::out_of_range& e) {
+		std::cout << e.what() << std::endl;
+	}
+	try {
+		v3.at(10);
+	}
+	catch(std::out_of_range& e) {
+		std::cout << e.what() << std::endl;
+	}
+
+	std::cout << v1.max_size() << std::endl;
 	return 0;
 }
