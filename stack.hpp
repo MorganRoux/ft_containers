75fe30b1385318031ceeb7d91f207ca617590059
Morgan Roux <roux.morgan@gmail.com>
2021-07-18 10:00:34 +0200

    stack need test

--- stack.hpp
+++ stack.hpp
@@ -6,7 +6,7 @@
 /*   By: mroux <mroux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/18 09:47:54 by mroux             #+#    #+#             */
-/*   Updated: 2021/07/18 09:53:19 by mroux            ###   ########.fr       */
+/*   Updated: 2021/07/18 09:59:32 by mroux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,11 @@
 #include <stdexcept>
 #include <cstddef>
 #include <typeinfo>
-#include "ft_iterator.hpp"
 #include "metafunctions.hpp"
 #include "vector.hpp"
 
 namespace ft
 {
-
-	template <class U>
-	class ra_iterator : public ft::iterator<random_access_iterator_tag, U>
-	{
-	};
-
 	template <class T, class Container = ft::vector<T>>
 	class stack
 	{
@@ -36,30 +29,33 @@ namespace ft
 		typedef Container container_type;
 		typedef size_type typename container_type::size_type;
 
-		explicit stack(const container_type &ctnr = container_type());
-		bool empty() const;
-		size_type size() const;
-		value_type &top();
-		const value_type &top() const;
-		void push(const value_type &val);
-		void pop();
+		explicit stack(const container_type &ctnr = container_type()): _cntr(cntr) {};
+		bool empty() const { return cntr.empty();}
+		size_type size() const { return cntr.size();}
+		value_type &top() { return cntr.back(); }
+		const value_type &top() const { return cntr.back(); }
+		void push(const value_type &val) { cntr.push_back(val); }
+		void pop() { cntr.pop_back(); }
+
+	private:
+		container_type cntr;
 	};
 
 	template <class T, class Container>
-	bool operator==(const stack<T, Container> &lhs, const stack<T, Container> &rhs);
+	bool operator==(const stack<T, Container> &lhs, const stack<T, Container> &rhs) { return (lhs.cntr == rhs.cntr); }
 
 	template <class T, class Container>
-	bool operator!=(const stack<T, Container> &lhs, const stack<T, Container> &rhs);
+	bool operator!=(const stack<T, Container> &lhs, const stack<T, Container> &rhs) { return !(lhs == rhs); }
 
 	template <class T, class Container>
-	bool operator<(const stack<T, Container> &lhs, const stack<T, Container> &rhs);
+	bool operator<(const stack<T, Container> &lhs, const stack<T, Container> &rhs) { return (lhs.cntr < rhs.cntr); }
 
 	template <class T, class Container>
-	bool operator<=(const stack<T, Container> &lhs, const stack<T, Container> &rhs);
+	bool operator<=(const stack<T, Container> &lhs, const stack<T, Container> &rhs) { return !(lhs > rhs); }
 
 	template <class T, class Container>
-	bool operator>(const stack<T, Container> &lhs, const stack<T, Container> &rhs);
+	bool operator>(const stack<T, Container> &lhs, const stack<T, Container> &rhs) { return (rhs < lhs); }
 
 	template <class T, class Container>
-	bool operator>=(const stack<T, Container> &lhs, const stack<T, Container> &rhs);
+	bool operator>=(const stack<T, Container> &lhs, const stack<T, Container> &rhs) { return !(lhs < rhs); }
 }
