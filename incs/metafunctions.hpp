e7bce3b31a52e0d3af4b0738c17f09fad708e996
Morgan Roux <roux.morgan@gmail.com>
2021-07-19 21:19:29 +0200

    tested lexical_compare and equal

--- incs/metafunctions.hpp
+++ incs/metafunctions.hpp
@@ -6,7 +6,7 @@
 /*   By: mroux <mroux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 15:59:55 by mroux             #+#    #+#             */
-/*   Updated: 2021/07/19 20:45:06 by mroux            ###   ########.fr       */
+/*   Updated: 2021/07/19 21:06:49 by mroux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -149,71 +149,7 @@ namespace ft
 	template<class T>
 	struct is_integral : is_integral_base<typename ft::remove_cv<T>::type > {};
 
-	// ================================ equal ================================ //
-	template <class InputIterator1, class InputIterator2>
-	bool equal(InputIterator1 first1, InputIterator1 last1, InputIterator2 first2)
-	{
-		while (first1 != last1)
-		{
-			if (!(*first1 == *first2))
-				return false;
-			++first1;
-			++first2;
-		}
-		return true;
-	}
 
-	template <class InputIterator1, class InputIterator2, class BinaryPredicate>
-	bool equal(InputIterator1 first1, InputIterator1 last1,
-			   InputIterator2 first2, BinaryPredicate pred)
-	{
-		while (first1 != last1)
-		{
-			if (!pred(*first1, *first2))
-				return false;
-			++first1;
-			++first2;
-		}
-		return true;
-	}
-
-	// ============================== lexicographical_compare ==================================== //
-
-	template <class InputIterator1, class InputIterator2>
-	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
-								 InputIterator2 first2, InputIterator2 last2)
-	{
-		while (first1 != last1)
-		{
-			if (first2 == last2 || *first2 < *first1)
-				return false;
-			else if (*first1 < *first2)
-				return true;
-			++first1;
-			++first2;
-		}
-		return (first2 != last2);
-	}
-	template <class InputIterator1, class InputIterator2, class Compare>
-	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
-								 InputIterator2 first2, InputIterator2 last2,
-								 Compare comp)
-	{
-		while (comp(first1, last1) || comp(last1, first1))
-		{
-			if (
-				(!comp(first2, last2) && !comp(last2, first2)) // first2 == last2
-				||
-				comp(*first2, *first1) //*first2 < *first1
-			)
-				return false;
-			else if (comp(*first1, *first2)) // //*first1 < *first2
-				return true;
-			++first1;
-			++first2;
-		}
-		return (comp(first2, last2) || comp(last2, first2)); //first2 != last2);
-	}
 
 	// ============================ is_iterator ===========================//
 	template <typename isIterator>
--- srcs/test_other.cpp
+++ srcs/test_other.cpp
@@ -4,7 +4,20 @@
 #include "utils.hpp"
 #include "metafunctions.hpp"
 #include "pair.hpp"
+#include "compare.hpp"
+#include "vector.hpp"
+#include <vector>
+#include <algorithm>
 
+bool mycomp(int a, int b)
+{
+	return a < b;
+}
+
+bool mypred(int a, int b)
+{
+	return a == b;
+}
 
 void test_other()
 {
@@ -24,16 +37,33 @@ void test_other()
 		std::cout << p1.first << p1.second << std::endl;
 		std::cout << p2.first << p2.second << std::endl;
 		std::cout << p3.first << p3.second << std::endl;
-		std::cout << NAMESPACE::make_pair(7,8).first << NAMESPACE::make_pair(7,8).second << std::endl;
-		p1 = NAMESPACE::make_pair(7,8);
+		std::cout << NAMESPACE::make_pair(7, 8).first << NAMESPACE::make_pair(7, 8).second << std::endl;
+		p1 = NAMESPACE::make_pair(7, 8);
 		std::cout << (p1 == p2) << std::endl;
 		std::cout << (p1 != p2) << std::endl;
 		std::cout << (p1 <= p2) << std::endl;
 		std::cout << (p1 >= p2) << std::endl;
-		std::cout << (p1 < p2 ) << std::endl;
-		std::cout << (p1 > p2 ) << std::endl;
-
+		std::cout << (p1 < p2) << std::endl;
+		std::cout << (p1 > p2) << std::endl;
 	}
 
+	std::cout << "lexical compare" << std::endl;
+	{
+		NAMESPACE::vector<int> v1, v2;
 
+		v1.push_back(1);
+		v1.push_back(2);
+		v1.push_back(3);
+		v1.push_back(4);
+		v2.push_back(1);
+		v2.push_back(2);
+		v2.push_back(5);
+
+		NAMESPACE::vector<int> v3 = v2;
+		std::cout << NAMESPACE::lexicographical_compare(v1.begin(), v1.end(), v2.begin(), v2.end()) << std::endl;
+		std::cout << NAMESPACE::lexicographical_compare(v2.begin(), v2.end(), v1.begin(), v1.end(), mycomp) << std::endl;
+		std::cout << NAMESPACE::equal(v1.begin(), v1.end(), v2.begin()) << std::endl;
+		std::cout << NAMESPACE::equal(v2.begin(), v2.end(), v1.begin()) << std::endl;
+		std::cout << NAMESPACE::equal(v3.begin(), v3.end(), v2.begin(), mypred) << std::endl;
+	}
 }
