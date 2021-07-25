5ff178c15c5cb5c83334b9ec160a67ad1975eb27
Morgan Roux <roux.morgan@gmail.com>
2021-07-25 11:27:46 +0200

    test ok with strings

--- srcs/test_map.cpp
+++ srcs/test_map.cpp
@@ -6,7 +6,7 @@
 /*   By: mroux <mroux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/19 21:19:57 by mroux             #+#    #+#             */
-/*   Updated: 2021/07/25 10:33:00 by mroux            ###   ########.fr       */
+/*   Updated: 2021/07/25 11:27:28 by mroux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -266,6 +266,236 @@ void test_map()
 
 			second = first;				   // second now contains 3 ints
 			first = std::map<char, int>(); // and first is now empty
+
+			std::cout << "Size of first: " << first.size() << '\n';
+			std::cout << "Size of second: " << second.size() << '\n';
+
+		}
+	}
+
+	std::cout << "=============== with strings =============" << std::endl;
+	{
+		NAMESPACE::map<std::string, std::string> m;
+		NAMESPACE::map<std::string, std::string>::iterator it;
+		NAMESPACE::map<std::string, std::string>::reverse_iterator rit;
+
+		it = m.insert(NAMESPACE::pair<std::string, std::string>("4", "4")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("1", "1")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("8", "8")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("5", "5")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("3", "3")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("2", "2")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("3", "6")).first;
+		std::cout << (*it).second << std::endl;
+
+		std::cout << "iterator" << std::endl;
+		for (it = m.begin(); it != m.end(); it++)
+			std::cout << (*it).first << (*it).second << std::endl;
+		std::cout << "reverse iterator" << std::endl;
+
+		for (rit = m.rbegin(); rit != m.rend(); rit++)
+			std::cout << (*rit).first << (*rit).second << std::endl;
+
+		NAMESPACE::map<std::string, std::string>::const_iterator cit(it);
+		for (cit = m.begin(); cit != m.end(); cit++)
+			std::cout << (*cit).first << cit->second << std::endl;
+		it->second = "1000";
+		// Should not compile
+		// cit->second = 3;
+		std::cout << "operator ->" << std::endl;
+		std::cout << it->second << cit->second << std::endl;
+
+		std::cout << "erase by position and erase by key" << std::endl;
+		print_map(m);
+		it = m.begin();
+		it++;
+		m.erase(it);
+		m.erase("3");
+		std::cout << "---" << std::endl;
+		print_map(m);
+
+		it = m.insert(NAMESPACE::pair<std::string, std::string>("14", "4")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("11", "1")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("18", "8")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("15", "5")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("13", "3")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("12", "2")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("13", "6")).first;
+		std::cout << (*it).second << std::endl;
+
+		print_map(m);
+		//m.analyse_map(10);
+		std::cout << "-1--" << std::endl;
+		it = m.begin();
+		std::cout << "delete " << it->first << std::endl;
+		m.erase(it);
+		print_map(m);
+		//m.analyse_map(10);
+		std::cout << "-2--" << std::endl;
+		it = m.begin();
+		std::cout << "delete " << it->first << std::endl;
+		m.erase(it);
+		print_map(m);
+		//m.analyse_map(10);
+		std::cout << "-3--" << std::endl;
+		it = m.begin();
+		std::cout << "delete " << it->first << std::endl;
+		m.erase(it);
+		print_map(m);
+		//m.analyse_map(10);
+		std::cout << "--4-" << std::endl;
+		it = m.begin();
+		std::cout << "delete " << it->first << std::endl;
+		m.erase(it);
+		print_map(m);
+		//m.analyse_map(10);
+		std::cout << "-5--" << std::endl;
+		it = m.begin();
+		std::cout << "delete " << it->first << std::endl;
+		m.erase(it);
+		print_map(m);
+		//m.analyse_map(10);
+		std::cout << "-6--" << std::endl;
+		it = m.begin();
+		std::cout << "delete " << it->first << std::endl;
+		m.erase(it);
+		print_map(m);
+		//m.analyse_map(10);
+		std::cout << "--7-" << std::endl;
+		it = m.begin();
+		std::cout << "delete " << it->first << std::endl;
+		m.erase(it);
+		print_map(m);
+		std::cout << "-8--" << std::endl;
+		it = m.begin();
+		std::cout << "delete " << it->first << std::endl;
+		m.erase(it);
+		print_map(m);
+		std::cout << "--9-" << std::endl;
+		it = m.begin();
+		m.erase(it);
+		print_map(m);
+		//m.analyse_map(10);
+		std::cout << "-10--" << std::endl;
+		it = m.begin();
+		m.erase(it);
+		print_map(m);
+		//m.analyse_map(10);
+
+		// Bug of the std::map
+		// std::cout << "---" << std::endl;
+		// it = m.begin();
+		// m.erase(it);
+		// print_map(m);
+		// std::cout << "already empty---" << std::endl;
+		// it = m.begin();
+		// m.erase(it);
+		// print_map(m);
+
+		std::cout << "erasing root" << std::endl;
+		it = m.insert(NAMESPACE::pair<std::string, std::string>("4", "4")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("1", "1")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("8", "8")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("5", "5")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("3", "3")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("2", "2")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("9", "9")).first;
+		std::cout << (*it).second << std::endl;
+		print_map(m);
+		m.erase("5");
+		print_map(m);
+
+		std::cout << "erase range" << std::endl;
+		it = m.insert(NAMESPACE::pair<std::string, std::string>("14", "4")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("11", "1")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("18", "8")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("15", "5")).first;
+		std::cout << (*it).second << std::endl;
+		it = m.insert(NAMESPACE::make_pair<std::string, std::string>("13", "3")).first;
+		print_map(m);
+		{
+			NAMESPACE::map<std::string, std::string>::iterator itkb = m.begin();
+			NAMESPACE::map<std::string, std::string>::iterator itke = m.end();
+			itkb++;
+			itkb++;
+			itke--;
+			itke--;
+			itke--;
+			std::cout << "---" << std::endl;
+			m.erase(itkb, itke);
+			print_map(m);
+		}
+
+		std::cout << "bounds" << std::endl;
+		std::cout << (*(m.lower_bound("3"))).second << std::endl;
+		std::cout << (*(m.upper_bound("3"))).second << std::endl;
+		std::cout << (*(m.equal_range("3").first)).second << std::endl;
+		std::cout << m.count("5") << m.count("10") << std::endl;
+		std::cout << m["5"] << m["10"] << std::endl;
+
+		std::cout << "clear" << std::endl;
+		{
+			NAMESPACE::map<std::string, std::string> m2 = m;
+			m2.clear();
+			print_map(m2);
+		}
+
+		std::cout << "swap" << std::endl;
+		{
+			NAMESPACE::map<std::string, std::string> m2;
+			it = m2.insert(NAMESPACE::pair<std::string, std::string>("8", "8")).first;
+			std::cout << (*it).second << std::endl;
+			it = m2.insert(NAMESPACE::make_pair<std::string, std::string>("11", "1")).first;
+			std::cout << (*it).second << std::endl;
+			it = m2.insert(NAMESPACE::make_pair<std::string, std::string>("81", "8")).first;
+			std::cout << (*it).second << std::endl;
+			it = m2.insert(NAMESPACE::make_pair<std::string, std::string>("51", "5")).first;
+			std::cout << (*it).second << std::endl;
+			it = m2.insert(NAMESPACE::make_pair<std::string, std::string>("31", "3")).first;
+			std::cout << (*it).second << std::endl;
+			it = m2.insert(NAMESPACE::make_pair<std::string, std::string>("12", "2")).first;
+			std::cout << (*it).second << std::endl;
+			it = m2.insert(NAMESPACE::make_pair<std::string, std::string>("19", "9")).first;
+
+			print_map(m);
+			print_map(m2);
+			m.swap(m2);
+			std::cout << "ok" << std::endl;
+			print_map(m);
+			print_map(m2);
+		}
+		std::cout << "operator=" << std::endl;
+		{
+			std::map<std::string, std::string> first;
+			std::map<std::string, std::string> second;
+
+			first["x"] = "8";
+			first["y"] = "16";
+			first["z"] = "32";
+
+			second = first;				   // second now contains 3 ints
+			first = std::map<std::string, std::string>(); // and first is now empty
 
 			std::cout << "Size of first: " << first.size() << '\n';
 			std::cout << "Size of second: " << second.size() << '\n';
