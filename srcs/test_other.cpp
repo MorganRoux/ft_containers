fc392318c2803a811b67b4fe5fb0521413a77e3c
Morgan@VM <mroux@student.42.fr>
2021-08-29 16:36:57 +0200

    ready to push

--- srcs/test_other.cpp
+++ srcs/test_other.cpp
@@ -33,13 +33,14 @@ class A
 
 void test_other()
 {
+	/*
 	std::cout << "Is Integral" << std::endl;
 	std::cout << NAMESPACE::is_integral<ft::vector<int> >::value << '\n';
 	std::cout << NAMESPACE::is_integral<float>::value << '\n';
 	std::cout << NAMESPACE::is_integral<signed char>::value << '\n';
 	std::cout << NAMESPACE::is_integral<const int>::value << '\n';
 	std::cout << NAMESPACE::is_integral<bool>::value << '\n';
-
+*/
 
 	std::cout << "Pairs" << std::endl;
 	{
